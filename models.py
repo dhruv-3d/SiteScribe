32079b33422c83dc95c6be0c0d696a3d0dc1ab91
Dhruv Desai <30370815+dhruv-3d@users.noreply.github.com>
2024-09-12 02:23:23 +0530

    fixed models.py

--- models.py
+++ models.py
@@ -3,16 +3,13 @@ from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-# from langchain_huggingface import HuggingFaceEmbeddings
-
-# GEMINI_API_KEY = os.environ['GOOGLE_AI_API_KEY']
-GEMINI_API_KEY = 'AIzaSyBQs_GjbITQaL54X6FofOa43dPAvqNT2CM'
+GEMINI_API_KEY = os.environ['GOOGLE_AI_API_KEY']
 
 llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=GEMINI_API_KEY)
+embeddings_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GEMINI_API_KEY)
 
 # model_name = 'qwen2:1.5b-instruct'
 # llm = Ollama(model=model_name, base_url='https://68e3-103-241-244-6.ngrok-free.app')
-
 
 # emb_model_name = "jinaai/jina-embeddings-v2-base-en"
 # emb_model_kwargs = {'device': 'cpu'}
@@ -23,9 +20,6 @@ llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=GEMINI_API_KEY)
 #     model_kwargs=emb_model_kwargs,
 #     encode_kwargs=emb_encode_kwargs
 # )
-
-embeddings_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GEMINI_API_KEY)
-
 
 def get_ai_resp(prompt):
     response = llm.invoke(prompt)
